d487bb3f73f940015d5e7227cf1fd5303cb08e41
zakimal <oza777you@gmail.com>
2018-12-06 01:13:11 +0900

    implemented mysh and organize directories neatly

--- ex-3/builtins.h
+++
@@ -1,3 +0,0 @@
-extern double Average(double x[], int n);
-extern double StdDev(double x[], int n);
-extern double Sum(double x[], int n);
--- ex-3/ws/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void* my_malloc(size_t sz);
-void* my_realloc(void *ptr, size_t sz);
-
-#endif
